0250f1d1179e6e5be2df4c84bef7b9e703687c34
joern@existsolutions.com <9az.xmJWY_d~>
2018-03-02 18:53:42 +0100

    split packagemanager into actual component and an app wrapper (top toolbar)

--- gulpfile.js
+++ gulpfile.js
@@ -102,7 +102,7 @@ gulp.task('deploy', ['deploy:other', 'deploy:components', 'deploy:styles'])
 gulp.task('watch', function () {
     gulp.watch('resources/css/!*', ['deploy:styles'])
     gulp.watch(otherPaths, ['deploy:other'])
-    gulp.watch('*.html', ['deploy:components'])
+    gulp.watch('**/*.html', ['deploy:components'])
 })
 
 gulp.task('default', ['watch'])
